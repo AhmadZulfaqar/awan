6ad02680b8bda25b376d353dab1a6570de35b261
DESKTOP-S8NONQ6\akarumbi <githubzulfaqar@gmail.com>
2017-08-29 10:26:53 +0800

    first init

---
+++ MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/HomeFragment.java
@@ -0,0 +1,65 @@
+package com.example.akarumbi.myintranetapps;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home_fragment, container, false);
+
+        Button btnprofile = (Button) rootView.findViewById(R.id.btn_profile);
+        btnprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProfileFragment profileFragment = new ProfileFragment();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.frame, profileFragment,profileFragment.getTag()).commit();
+            }
+        });
+
+        Button btnmytimesheet = (Button) rootView.findViewById(R.id.btn_my_timesheet);
+        btnmytimesheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimesheetFragment timesheetFragment = new TimesheetFragment();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.frame, timesheetFragment,timesheetFragment.getTag()).commit();
+            }
+        });
+
+        Button btnlms = (Button) rootView.findViewById(R.id.btn_lms);
+        btnlms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LmsFragment lmsFragment = new LmsFragment();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.frame, lmsFragment,lmsFragment.getTag()).commit();
+            }
+        });
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+
+}
---
+++ MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.akarumbi.myintranetapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_login;
+    private TextView txtforgotpassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = (Button)findViewById(R.id.btn_login);
+        txtforgotpassword = (TextView)findViewById(R.id.txtForgotPassword);
+        txtforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this,ForgotPassword.class);
+                startActivity(in);
+            }
+        });
+
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent next = new Intent(MainActivity.this,NavDrawer.class);
+        startActivity(next);
+    }
+}

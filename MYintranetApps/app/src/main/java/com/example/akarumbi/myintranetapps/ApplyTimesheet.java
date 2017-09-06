014276350600ccdfed76ba63bd71d5d35824e490
nasywanmashud <nasywanmashud@gmail.com>
2017-09-06 16:20:39 -0700

    1
    1

--- MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/ApplyTimesheet.java
+++ MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/ApplyTimesheet.java
@@ -2,12 +2,49 @@ package com.example.akarumbi.myintranetapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class ApplyTimesheet extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView theDate;
+
+    String [] Project={"E-Aduan" , "Firefly (R&D)" , "Firefly Corporate Website" , "Firefly Corporate Website" , "Firefly Non-Billable (Kiosk,SMS, Mobile, Enrich)" ,
+                           "Firefly T&M Billable (Skysales, Intranet, Extranet, FLIRT)" , "Internal Tasks (Intern - Altriz)" , "Internal Tasks (Intern - ME-Tech" ,
+                           "Internal Tasks (Technical)" , "NIDA Rooms GST/VAT Tax Invoice System" , "RSOG Website & Intranet Maintenance Only" , "TBD (R&D)" ,
+                           "TBD Corp Website" , "TBD Mobile App" , "TBD Web Redemption" } ;
+
+    String [] Task={ "Administrative Tasks" , " Annual Leave" , "Attend Training (External)" , "Attend Training (Internal)" , "Break time, solat and staff recreation." ,
+            "Client Meeting (Outside)" , "Company Event/Gathering" , "Company meeting" , "Conduct training & preparation" , "Emergency Leave" ,"Enter & approve timesheet" ,
+            "Friday prayers" , " Internal system/product development & enhancement and support" , "Maternity Leave" , "Medical Leave"};
+
+    String [] Status= {"Pending" , "Submitted"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_timesheet);
+
+        ArrayAdapter<String > arrayAdapter1= new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,Project);
+        MaterialBetterSpinner betterSpinner1 = (MaterialBetterSpinner) findViewById(R.id.Project);
+        betterSpinner1.setAdapter(arrayAdapter1);
+
+        ArrayAdapter<String > arrayAdapter2= new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,Task);
+        MaterialBetterSpinner betterSpinner2 = (MaterialBetterSpinner) findViewById(R.id.Task);
+        betterSpinner2.setAdapter(arrayAdapter2);
+
+        ArrayAdapter<String > arrayAdapter3= new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,Status);
+        MaterialBetterSpinner betterSpinner3 = (MaterialBetterSpinner) findViewById(R.id.Status);
+        betterSpinner3.setAdapter(arrayAdapter3);
+
+
+
     }
 }
--- MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/TimesheetFragment.java
+++ MYintranetApps/app/src/main/java/com/example/akarumbi/myintranetapps/TimesheetFragment.java
@@ -1,11 +1,16 @@
 package com.example.akarumbi.myintranetapps;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 
@@ -16,6 +21,7 @@ public class TimesheetFragment extends Fragment {
 
     private static final String TAG = "TimesheetFragment";
     private CalendarView mCalendarview;
+
     public TimesheetFragment() {
         // Required empty public constructor
     }
@@ -25,7 +31,32 @@ public class TimesheetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.timesheet_fragment, container, false);
+//        return inflater.inflate(R.layout.timesheet_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.timesheet_fragment, container, false);
+
+
+        mCalendarview  = (CalendarView) rootView.findViewById(R.id.mCalendarview);
+        mCalendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1+1) + "/" + i2 + "/" + i;
+                Log.d(TAG, "onSelectedDayChange: date: mm/dd/yyyy " + date);
+                Intent in = new Intent(getActivity(),ApplyTimesheet.class);
+                startActivity(in);            }
+        });
+//        btnlms.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                LmsFragment lmsFragment = new LmsFragment();
+//                FragmentManager manager = getFragmentManager();
+//                manager.beginTransaction().replace(R.id.frame, lmsFragment,lmsFragment.getTag()).commit();
+
+
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+
     }
 //
 }
